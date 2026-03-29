63f6ef15b938293dbd3df5a3f4519a2ffd165e3a
Mario Esteban Mateo <memateo@gmail.com>
2026-03-29 17:16:24 -0300

    fix: Allow Vite to use next available port if 5173 is busy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    strictPort: true,
     proxy: {
       "/api": {
         target: "https://api.thegamesdb.net/v1",
